3eddc79213c2e7d173f1a3cbb1efbcc58fccf2f9
Dmitrii Aksenov <aksbum@gmail.com>
2025-06-23 18:03:30 +0300

    deploy on gitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import "@fontsource/roboto"; // подключает Roboto
 
 import App from './App.tsx'
-import React from 'react';
 import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
 const theme = createTheme({
